5de263be86431e19c969ab311c9b9d9840881b31
pierrebazin <pierre.bazin.sii@gmail.com>
2023-07-21 17:05:41 +0200

    feat: error message when login error

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../providers/auth.service";
 import {Router} from "@angular/router";
 import {UserService} from "../providers/user.service";
+import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-login',
@@ -11,10 +12,12 @@ import {UserService} from "../providers/user.service";
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  isLoading: boolean = false;
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
+    private snackBar: MatSnackBar,
     private userService: UserService,
     private authService: AuthService
   ) {
@@ -26,19 +29,27 @@ export class LoginComponent {
 
   onSubmit(): void {
     if (this.loginForm.valid) {
-      const { email, password } = this.loginForm.value;
-      // TODO deprecated, comprend pas pq
+      this.isLoading = true;
+      const {email, password} = this.loginForm.value;
       this.authService.login(email, password).subscribe(
-        (response) => {
-          const token = response.token;
-          this.authService.storeToken(token);
-          this.userService.setCurrentUser().subscribe();
-          this.router.navigate(['/gallery']).then();
-        },
-        (error) => {
-          console.error(error);
-        }
+        (response) => this.loginSuccess(response),
+        (error) => this.loginError(error),
+        () => this.isLoading = false
       );
     }
+  }
+
+  loginSuccess(response: any) {
+    const token = response.token;
+    this.authService.storeToken(token);
+    this.userService.setCurrentUser().subscribe();
+    this.router.navigate(['/gallery']).then();
+  }
+
+  loginError(error: any) {
+    this.snackBar.open('There is no user record corresponding to this identifier or the password is incorrect', undefined, {
+      panelClass: 'snackbar-error',
+      duration: 5000,
+    });
   }
 }
