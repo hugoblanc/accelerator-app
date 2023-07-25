c8a191bdcc9ecf8eb5dea9b471c974dc965f0814
Hugo Blanc <hugo.blanc@agicap.com>
2023-07-25 21:54:36 +0200

    feat: auto scroll bottom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from "./providers/auth.service";
-import {UserService} from "./providers/user.service";
-import {Router} from "@angular/router";
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AuthService } from "./providers/auth.service";
+import { UserService } from "./providers/user.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -10,12 +10,27 @@ import {Router} from "@angular/router";
 })
 export class AppComponent implements OnInit {
   constructor(private readonly authService: AuthService,
-              private router: Router,
-              public readonly userService: UserService) {
+    private router: Router,
+    public readonly userService: UserService) {
   }
   ngOnInit() {
     if (this.authService.isLoggedIn()) {
       this.userService.setCurrentUser().subscribe();
     }
+    this.scrollToBottom();
+  }
+
+  @ViewChild('scrollMe') private myScrollContainer!: ElementRef;
+
+
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
   }
 }
