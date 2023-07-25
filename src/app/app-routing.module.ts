3f8bfd986d240618795e0bbeee4b819f0a1dad63
pierrebazin <pierre.bazin.sii@gmail.com>
2023-07-25 10:42:03 +0200

    fix : put gallery has main screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: '', redirectTo: 'gallery', pathMatch: 'full' },
 
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
 
