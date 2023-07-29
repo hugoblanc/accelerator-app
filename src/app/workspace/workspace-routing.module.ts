4f104d6fb7d19a43efdf6b05b64d7f25401dc0fa
Hugo Blanc <hugo.blanc@agicap.com>
2023-07-29 13:28:30 +0200

    fix: navigation

--- src/app/workspace/workspace-routing.module.ts
+++ src/app/workspace/workspace-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WorkspaceCreateComponent } from "./workspace-create/workspace-create.component";
 import { WorkspaceHomeComponent } from "./workspace-home/workspace-home.component";
+import { WorkspaceComponent } from './workspace.component';
 
 const routes: Routes = [
   {
-    path: '', component: WorkspaceHomeComponent, children: [
+    path: '', component: WorkspaceComponent, children: [
+      { path: '', component: WorkspaceHomeComponent },
       { path: 'create', component: WorkspaceCreateComponent },
     ]
   }];
