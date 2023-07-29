7c3f38c8b8c1ff210191f6ca2aa6c0cdcc0e4803
Pierre Bazin <bazinpierre76@icloud.com>
2023-07-29 19:09:38 +0200

    feat: change routing for workspace home

--- src/app/workspace/workspace-home/workspace-home.module.ts
+++ src/app/workspace/workspace-home/workspace-home.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {WorkspaceHomeComponent} from "./workspace-home.component";
-import {WorkspaceHomeRoutingModule} from "./workspace-home-routing.module";
 import { TeamsComponent } from './teams/teams.component';
 import { MembersComponent } from './members/members.component';
 import { TeamsTableComponent } from './teams/teams-table/teams-table.component';
@@ -9,6 +8,7 @@ import {MatTableModule} from "@angular/material/table";
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
@@ -20,11 +20,11 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     CommonModule,
-    WorkspaceHomeRoutingModule,
     MatTableModule,
     MatIconModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ]
 })
 export class WorkspaceHomeModule {
--- src/app/workspace/workspace-routing.module.ts
+++ src/app/workspace/workspace-routing.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { WorkspaceCreateComponent } from "./workspace-create/workspace-create.component";
-import { WorkspaceHomeComponent } from "./workspace-home/workspace-home.component";
-import { WorkspaceComponent } from './workspace.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {WorkspaceCreateComponent} from "./workspace-create/workspace-create.component";
+import {WorkspaceHomeComponent} from "./workspace-home/workspace-home.component";
+import {WorkspaceComponent} from './workspace.component';
+import {TeamsComponent} from "./workspace-home/teams/teams.component";
+import {MembersComponent} from "./workspace-home/members/members.component";
 
 const routes: Routes = [
+  {path: 'create', component: WorkspaceCreateComponent},
   {
-    path: '', component: WorkspaceComponent, children: [
-      { path: '', component: WorkspaceHomeComponent },
-      { path: 'create', component: WorkspaceCreateComponent },
+    path: 'home', component: WorkspaceHomeComponent, children: [
+      {path: '', component: TeamsComponent},
+      {path: 'teams', component: TeamsComponent},
+      {path: 'members', component: MembersComponent}
     ]
   }];
 
